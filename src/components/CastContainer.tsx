076a4c919823aaa6c056d114ef3c04c7457879a1
Kyungsuh Park <pyo05360@gmail.com>
2021-12-27 23:07:19 +0900

    📝 Cast 인물들 이미지 추가

--- src/components/CastContainer.tsx
+++ src/components/CastContainer.tsx
@@ -4,24 +4,80 @@ import styled from "@emotion/styled";
 import Link from 'next/link';
 import { MEDIA_QUERY_END_POINT, TYPES } from "../constants";
 import { Cast } from '../types/cast';
+import  he  from "../../public/img/he.png";
+import { castImage } from "../../src/constants";
+import Image from "next/image";
+// import Head from 'next/head'
 
 export const CastContainer = () => {
     const name = 'cast'
     const {data, error} = useData(TYPES[0]);
+
     if(error) return <Error />
     if(!data) return <Loading />
+
     return (
-        <div>
-            <h1>This is Cast Page!!!</h1>
-            {data.map((castData: Cast) => {
-                return (
-                    <div key = {castData.id}>
-                        <p>name: {castData.name}</p>
-                        <p>born: {castData.born}</p>
-                        <p>bio: {castData.bio.url}</p>
-                  </div>
-                )
-            })}   
-        </div>
+        <Container>
+                {data.map((castData: Cast) => {
+                    const { name, born, bio, id } = castData;
+                    return (
+                        <Card key={castData.id}>
+                            <CastImg src={castImage[id - 1].url} alt="캐스트 인물" />
+                            <h4>{castData.name}</h4>
+                            <p><span>Birth</span>: {castData.born}</p>
+                            <Link href={bio.url}><a>More</a></Link>
+                        </Card>
+                    );
+                })}
+        </Container>
       )
 }
+
+const CastImg = styled.img`
+  height: 70%;
+  border-radius: 10px;`
+
+const Container = styled.div`
+    margin: 80px auto 0;
+    /* max-width:1000px; */
+    width: 100%;
+    padding: 2rem 100px 2rem;
+    border: 1px solid black;
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(3, 2fr);
+    background-color: yellowgreen;
+    @media (min-width: ${MEDIA_QUERY_END_POINT.TABLET}) {
+    grid-template-columns: repeat(3, calc((100% - 2rem * 2) / 3));
+  }
+    h4 {
+        font-size: 1.3rem;
+        font-weight: 600;
+        
+    }
+    Image {
+        border-radius: 50%;
+    }
+`
+
+const Card = styled.div`
+    padding: 20px 0 0;
+    border: 3px solid black;
+    border-radius: 30px;
+    width: 250px;
+    height: 400px;
+    margin: 0 auto;
+    background-color: green;
+    overflow: hidden;
+    text-align: center;
+    line-height: 1.8;
+    span {
+        font-weight: 600;
+    }
+    a {
+        border-bottom: 1px solid;
+
+    }
+
+
+`
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,55 @@ export const API_ENDPOINT = "https://api.sampleapis.com/futurama/";
 export const TYPES = ['cast','characters','episodes','info','inventory','questions'];
 
 export const MEDIA_QUERY_END_POINT = {
-    MOBILE: '480px',
-    TABLET: '768px',
-    DESKTOP: '1024px'
-  }
+  MOBILE: '480px',
+  TABLET: '768px',
+  DESKTOP: '1024px'
+}
+
+export const castImage = [
+  { //빌리웨스트
+    url: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSjbQIfRvdAoahhMkj8VmccsZzQ2qvUI71Yv8fI1bKChf-jFxuQ0L_IvyCwwfUX",
+  },
+  { //카테이사갈
+    url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ-AFlSx_ZJipsRNBgsYZ2baVL8bh-GV6nWHgwiOFfhv5HaSnUT",
+  },
+  { // 존디미고
+    url: "https://m.media-amazon.com/images/M/MV5BMzI2NTk1MzYxOV5BMl5BanBnXkFtZTcwMDE2MDQ4Nw@@._V1_.jpg",
+  },
+  { //트레스 맥낼리
+    url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR0p7wNFQMgG3g5zI8eADwkpcfO_8HXr9aaLrHevjJVGNudlf4J",
+  },
+  { // 필 라말
+    url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTbYNPrAKLym31oCj94ksNtBeNIEDk0Tl3RPNBDIp4NORVeAU8A",
+  },
+  { // 로렌 톰 
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPSbO-9Y5LSfKTP1K_vFavfIZBIcO4bEGPrGI6oiD4wWW2QFdl",
+  },
+  {  //Maurice
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Q-2y7OtWCj_pF4MOBuxfuphQlEmzZMpp94hABYXiIGULVr9z",
+  },
+  { // David
+    url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQt-qOjiRVEWztrrR5iN3Y76c-dj496owDMkribPe3-n5R7LJY1",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWR-ZOzy5eo9JCGUoI6npwwFZeHv-Xo2dRnGBijnCy39KWyUoz",
+  },
+  { //Frank Welker
+    url: "https://www.giantbomb.com/a/uploads/scale_medium/1/10354/2592272-600full-frank-welker.jpg",
+  },
+  { //dawnn lewis
+    url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQUpKQ5hmSR6iQfqLLg7Y-gMRRI7LXyQHqSSX4ibbSxV0XgrFZ4",
+  },
+  {
+    url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTCgKahOJCWwoUzrtewLz0cJFxRD-HMNPw79CUx5wRW4BP_Ha4P",
+  },
+  {
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSr2KwH_TJsP-h9Ac25xHGEdV0DT6bvZ1cDQ&usqp=CAU",
+  },
+  {
+    url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS8_65ia2xHoR0_7gNrWj-s6bbnJHZ2qpjBJ7QLL0GO_KEEeoMF",
+  },
+  {
+    url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTJzMQ6KoUI_YXLTGuatkcT48yBjJEZocZaOzdGZeWTy8cc6dr0",
+  },
+];
