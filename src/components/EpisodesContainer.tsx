b48b5b24778deb1595c9e934f174f3d3d8223956
Kyungsuh Park <pyo05360@gmail.com>
2021-12-27 23:11:02 +0900

    💄 에피소드 css 추가

--- src/components/EpisodesContainer.tsx
+++ src/components/EpisodesContainer.tsx
@@ -12,47 +12,99 @@ export const EpisodesContainer = () => {
   if (!data) return <Loading />
 
   return (
-    <main>
-      <h2>{name}</h2>
-      <Ul>
-        {data.map((EpisodesData: Episodes) => {
-          const { number, title, writers, originalAirDate, desc, id } = EpisodesData;
-          return (
-            <li key={`futurama-cast-${id}`}>
-              <div>EP.{number}</div>
-              <h3>{title}</h3>
-              <dl>
-                <div>
-                  <dt>writers:</dt>
-                  <dd>{writers}</dd>
-                </div>
-                <div>
-                  <dt>originalAirDate:</dt>
-                  <dd>{originalAirDate}</dd>
-                </div>
-              </dl>
-              <p>{desc}</p>
-            </li>
-          )
-        })}
-      </Ul>
-    </main>
+      <div>
+        <Container>
+          {data.map((episode: Episodes) => {
+            const  {number, title, writers, originalAirDate, desc, id } = episode;
+
+            return (
+              <Card key={`episode-${id}`}> 
+              {/* <div>EP.{number}</div> */}
+                  <ul>
+                  <li><Item>Episode #{number}</Item></li>
+                  <Title>{title}</Title>
+                  <li><Item>Aired Date : </Item> {originalAirDate}</li>
+                  <li><Item>Writters : </Item> {writers}</li>
+                  <li><Desc><Item>Story :</Item> {desc}</Desc></li>
+                  </ul>
+              </Card>
+            )
+          })
+        }
+        </Container>
+      </div>
   )
 }
+const Container = styled.div`
+margin: 80px auto;
+padding-top: 2rem;
+padding: 100px;
+background-color: tomato;
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+align-items: center;
+justify-content: center;
+text-align: center;
 
-const Ul = styled.ul`
-  max-width:1000px; 
-  margin:100px auto;
-  display:grid;
-  gap :2rem;
-  li{}
-  dl{
-    display:flex;
-    justify-content:center;
-    margin-bottom:1rem;
-    gap:2rem;
-    flex-wrap:wrap;
+`
+
+const Card = styled.div`
+text-align: center;
+border: 4px solid black;
+border-radius: 50%;
+width: 400px;
+height: 400px;
+margin: 0 auto;
+
+margin-bottom: 30px;
+background-color: yellowgreen;
+display: flex;
+justify-content: center;
+padding-top: 300px;
+align-items: center;
+overflow: hidden;
+transition: 0.7s;
+li {
+  opacity: 0;
+}
+
+:hover {
+  padding-top: 0px;
+  overflow: hidden;
+  width: 550px;
+  height: 550px;
+  background: yellow;
+  li {
+    display: visible;
+    opacity: 1;
   }
-  dl + p{text-align:center; max-width:800px; margin:0 auto;}
+
+}
+
+
+overflow: hidden;
+ul {
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  line-height: 2rem;
+  font-size: 18px;
+}
+`
+
+const Title = styled.div`
+font-size: 2rem;
+font-weight: 600;
+margin-bottom: 2rem;
+`
+const Item = styled.div`
+display: inline-block;
+font-weight: 600;
+font-size: 1.2rem;
+`
+
+const Desc = styled.div`
 `
 
